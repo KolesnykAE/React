e23ff028562229beab0657a1c95d3e6b15b6e186
KolesnykAE <kolesnik.ae@gmail.com>
2021-04-29 23:15:53 +0300

    HW1-done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import CharacterComponent from "./Components/characters/characterComponent";
 
--- src/Components/characters/characterComponent.js
+++ src/Components/characters/characterComponent.js
@@ -2,11 +2,24 @@ import './characterComponent.css';
 
 function CharacterComponent(props) {
     console.log(props);
-    return <div>
-        <img src={props.image} alt=""/>
+
+    // let cls = '';
+    // if (props.name === 'Alina') {
+    //     cls = 'Alina-class';
+    // } else {
+    //     cls = 'Anna-class';
+    // }
+
+    // лучше записать вариант 2
+
+    let cls = props.name === 'Alina' ? 'Alina-class' : 'Anna-class';
+
+
+    return <div className={cls}>
         <h1>
             {props.name}
         </h1>
+        <img src={props.image} alt=""/>
         <p>
             {props.description}
         </p>
