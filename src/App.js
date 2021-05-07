fa557bff44d0e6f16478b50cc35da59418cd61af
KolesnykAE <kolesnik.ae@gmail.com>
2021-05-07 08:35:36 +0300

    home work 3 part1 (fetch)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import CharacterComponent from "./Components/characters/characterComponent";
+import Users from "./Components/users/Users";
 
 
 function App() {
     return (
         <div>
-            <CharacterComponent
-                name={'Alina'}
-                image={'https://i.pinimg.com/originals/f3/73/7e/f3737e27e9f5e7632204d4a90bd03a45.png'}
-                description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus ducimus eaque, nisi obcaecati odit, pariatur quasi quia quos reprehenderit sequi soluta tenetur velit vero voluptatibus. Consectetur delectus iusto tenetur?'}/>
-            <CharacterComponent
-                name={'Anna'}
-                image={'https://spar.org.ua/img.php?ipt=https://cdnimg.rg.ru/img/content/201/71/93/shcherbakova_d_850.jpg'}
-                description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus ducimus eaque, nisi obcaecati odit, pariatur quasi quia quos reprehenderit sequi soluta tenetur velit vero voluptatibus. Consectetur delectus iusto tenetur?'}/>
+            <Users/>
 
         </div>
     );
